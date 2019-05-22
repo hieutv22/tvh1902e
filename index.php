feb23326c8b136607bf202c9d49d5c6e3ad58645
hieutv22 <vanhieuls1997@gmail.com>
2019-05-22 20:43:55 +0700

    up

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?
-echo "automatic deploy";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Câu lệnh điều kiện trong php</h1>
+<?php
+$age = 19; 
+if ($age > 18) {
+	echo "<br> Người trưởng thành";
+} else {
+	echo "<br> Trẻ em";
+}
+?>
+</body>
+</html>
